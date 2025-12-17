9a4f92c7e25bc11c4d1d14d9da861d9179088ff4
remybms <112947392+remybms@users.noreply.github.com>
2025-12-17 14:35:23 +0100

    [WIP] commit to go on WSL

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, Go !")
+	if err := godotenv.Load(); err != nil {
+        log.Println(".env file not found, using system environment variables")
+    }
+
+    user := "postgres"
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_HOST")
+    dbname := os.Getenv("DB_NAME")
+    port := os.Getenv("DB_PORT")
+
+	encodedPassword := url.QueryEscape(password)
+
+
+    databaseURL := fmt.Sprintf(
+        "postgresql://%s:%s@%s:%s/%s?sslmode=require",
+        user,
+        encodedPassword,
+        host,
+        port,
+        dbname,
+    )
+
+
+
+    fmt.Println("DATABASE_URL:", databaseURL)
+	conn, err := pgx.Connect(context.Background(), databaseURL)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	var version string
+	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
+		log.Fatalf("Query failed: %v", err)
+	}
+
+	log.Println("Connected to:", version)
 }
